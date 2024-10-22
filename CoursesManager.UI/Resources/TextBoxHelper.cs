77df79eb24829fda7b478d5d6e9c826620f73a2b
Jarne Kompier <jko@wigersma-sikkema.com>
2024-10-22 20:27:50 +0200

    In progress

---
+++ CoursesManager.UI/Resources/TextBoxHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace CoursesManager.UI.Resources;
+
+public static class TextBoxHelper
+{
+    public static readonly DependencyProperty PlaceholderTextProperty =
+        DependencyProperty.RegisterAttached(
+            "PlaceholderText",
+            typeof(string),
+            typeof(TextBoxHelper),
+            new PropertyMetadata(string.Empty));
+
+    public static string GetPlaceholderText(DependencyObject obj)
+    {
+        return (string)obj.GetValue(PlaceholderTextProperty);
+    }
+
+    public static void SetPlaceholderText(DependencyObject obj, string value)
+    {
+        obj.SetValue(PlaceholderTextProperty, value);
+    }
+}
