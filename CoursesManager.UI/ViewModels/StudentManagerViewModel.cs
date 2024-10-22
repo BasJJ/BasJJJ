98af97a3b05ee3dcc6363fad552e7cda306c0fe0
Jarno Gerrets <152329688+JarnoGerrets@users.noreply.github.com>
2024-10-22 13:32:38 +0200

    Added app setup
    Author, Jarne Gerrets

---
+++ CoursesManager.UI/ViewModels/StudentManagerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CoursesManager.MVVM.Data;
+using CoursesManager.MVVM.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursesManager.UI.ViewModels
+{
+    public class StudentManagerViewModel : NavigatableViewModel
+    {
+        public StudentManagerViewModel(INavigationService navigationService) : base(navigationService)
+        {
+        }
+    }
+}
