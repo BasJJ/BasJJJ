956fb342b153671758fb30be2aec5cfbc4c526b8
Jarne Kompier <j.kompier@hotmail.nl>
2024-10-31 23:09:50 +0100

    Finished unit tests for mvvm

---
+++ CoursesManager.MVVM/Messages/IRecipient.cs
@@ -0,0 +1,6 @@
+﻿namespace CoursesManager.MVVM.Messages;
+
+internal interface IRecipient<TRecipient>
+{
+    TRecipient Recipient { get; set; }
+}
---
+++ CoursesManager.MVVM/Messages/Subscriber.cs
@@ -0,0 +1,8 @@
+﻿namespace CoursesManager.MVVM.Messages;
+
+internal class Subscriber<TMessageType, TRecipient>(Action<TMessageType> handler, TRecipient recipient) : IHandler<TMessageType>, IRecipient<TRecipient>
+{
+    public Action<TMessageType> Handler { get; set; } = handler;
+
+    public TRecipient Recipient { get; set; } = recipient;
+}
