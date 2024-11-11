9c53ed4b2459bb76a19fb0c71bcb860349a58925
Jarno Gerrets <jarnogerrets@gmail.com>
2024-11-11 01:04:55 +0100

    added GlobalCache

--- CoursesManager.MVVM/GlobalCache/GlobalCache.cs
+++ CoursesManager.MVVM/GlobalCache/GlobalCache.cs
@@ -1,16 +1,21 @@
-﻿public class GlobalCache
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+public class GlobalCache
 {
     private readonly int _capacity;
-    private readonly Dictionary<string, LinkedListNode<(string Key, object Value)>> _cacheMap;
-    private readonly LinkedList<(string Key, object Value)> _usageOrder;
+    private readonly ConcurrentDictionary<string, LinkedListNode<CacheItem>> _cacheMap;
+    private readonly LinkedList<CacheItem> _usageOrder;
+    private readonly object _lock = new object();
 
     private static readonly Lazy<GlobalCache> _instance = new Lazy<GlobalCache>(() => new GlobalCache(10));
 
     private GlobalCache(int capacity)
     {
         _capacity = capacity;
-        _cacheMap = new Dictionary<string, LinkedListNode<(string Key, object Value)>>();
-        _usageOrder = new LinkedList<(string Key, object Value)>();
+        _cacheMap = new ConcurrentDictionary<string, LinkedListNode<CacheItem>>();
+        _usageOrder = new LinkedList<CacheItem>();
     }
 
     public static GlobalCache Instance => _instance.Value;
@@ -20,37 +25,64 @@
         if (!_cacheMap.ContainsKey(key))
             throw new KeyNotFoundException();
 
-        var node = _cacheMap[key];
-        _usageOrder.Remove(node);
-        _usageOrder.AddFirst(node);
-
-        return node.Value.Value;
-    }
-
-    public void Put(string key, object value)
-    {
-        if (_cacheMap.ContainsKey(key))
+        lock (_lock)
         {
             var node = _cacheMap[key];
-            node.Value = (key, value);
             _usageOrder.Remove(node);
             _usageOrder.AddFirst(node);
+            return node.Value.Value;
         }
-        else
-        {
-            if (_cacheMap.Count >= _capacity)
-            {
-                var lruNode = _usageOrder.Last;
-                if (lruNode != null)
-                {
-                    _cacheMap.Remove(lruNode.Value.Key);
-                    _usageOrder.RemoveLast();
-                }
-            }
+    }
 
-            var newNode = new LinkedListNode<(string Key, object Value)>((key, value));
-            _usageOrder.AddFirst(newNode);
-            _cacheMap[key] = newNode;
+    public void Put(string key, object value, bool isPermanent)
+    {
+        lock (_lock)
+        {
+            if (_cacheMap.TryGetValue(key, out var existingNode))
+            {
+                existingNode.Value = new CacheItem(key, value, isPermanent);
+                _usageOrder.Remove(existingNode);
+                _usageOrder.AddFirst(existingNode);
+            }
+            else
+            {
+                if (_cacheMap.Count >= _capacity)
+                {
+                    EvictNonPermanentItem();
+                }
+                var newNode = new LinkedListNode<CacheItem>(new CacheItem(key, value, isPermanent));
+                _usageOrder.AddFirst(newNode);
+                _cacheMap[key] = newNode;
+            }
+        }
+    }
+
+    private void EvictNonPermanentItem()
+    {
+        var node = _usageOrder.Last;
+        while (node != null)
+        {
+            if (!node.Value.IsPermanent)
+            {
+                _cacheMap.TryRemove(node.Value.Key, out _);
+                _usageOrder.Remove(node);
+                break;
+            }
+            node = node.Previous;
+        }
+    }
+
+    private class CacheItem
+    {
+        public string Key { get; }
+        public object Value { get; set; }
+        public bool IsPermanent { get; }
+
+        public CacheItem(string key, object value, bool isPermanent)
+        {
+            Key = key;
+            Value = value;
+            IsPermanent = isPermanent;
         }
     }
 }
