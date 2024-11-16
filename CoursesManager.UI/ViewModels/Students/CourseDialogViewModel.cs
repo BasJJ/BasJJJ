ebe80384b44019f8f3fb388ebc672f5867ee2230
BasWindesheim <152599986+BasWindesheim@users.noreply.github.com>
2024-11-16 11:43:12 +0100

    Styles toegevoegd
    Style voor combobox, datepicker, afbeelding upload en checkbox 10 toegevoegd. Onthouden dat checkbox 10 niet gemerged mag worden (Jarno)

--- CoursesManager.UI/ViewModels/Students/CourseDialogViewModel.cs
+++ CoursesManager.UI/ViewModels/Students/CourseDialogViewModel.cs
@@ -1,22 +1,31 @@
-﻿using CoursesManager.MVVM.Dialogs;
+﻿using CoursesManager.MVVM.Commands;
+using CoursesManager.MVVM.Dialogs;
 using CoursesManager.UI.Models;
+using CoursesManager.UI.Models.Repositories.CourseRepository;
+using CoursesManager.UI.Models.Repositories.RegistrationRepository;
+using CoursesManager.UI.Models.Repositories.StudentRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CoursesManager.UI.ViewModels.Students
 {
     class CourseDialogViewModel : DialogViewModel<Course>
     {
+
         public CourseDialogViewModel(Course? dialogResultType) : base(dialogResultType)
         {
+
+           
         }
 
         protected override void InvokeResponseCallback(DialogResult<Course> dialogResult)
         {
             ResponseCallback.Invoke(dialogResult);
         }
+
     }
 }
