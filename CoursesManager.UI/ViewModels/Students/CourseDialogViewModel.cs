bac0336950cfff0b098ed768cdb5b9d1237fbe97
BasWindesheim <152599986+BasWindesheim@users.noreply.github.com>
2024-11-17 15:09:21 +0100

    Voor Jarne
    Annuleer knop

--- CoursesManager.UI/ViewModels/Students/CourseDialogViewModel.cs
+++ CoursesManager.UI/ViewModels/Students/CourseDialogViewModel.cs
@@ -2,6 +2,7 @@
 using CoursesManager.MVVM.Dialogs;
 using CoursesManager.UI.Models;
 using CoursesManager.UI.Models.Repositories.CourseRepository;
+using CoursesManager.UI.Models.Repositories.LocationRepository;
 using CoursesManager.UI.Models.Repositories.RegistrationRepository;
 using CoursesManager.UI.Models.Repositories.StudentRepository;
 using System;
@@ -16,10 +17,31 @@ namespace CoursesManager.UI.ViewModels.Students
     class CourseDialogViewModel : DialogViewModel<Course>
     {
 
-        public CourseDialogViewModel(Course? dialogResultType) : base(dialogResultType)
+        private readonly ICourseRepository _courseRepository;
+        private readonly IRegistrationRepository _registrationRepository;
+        private readonly IDialogService _dialogService;
+        private readonly ILocationRepository _locationRepository;
+
+        public ICommand SaveCommand {  get;  }
+        public ICommand CancelCommandd { get; }
+        public Course Course { get; set; }
+
+
+        public CourseDialogViewModel(ICourseRepository courseRepository,  IDialogService dialogService, ILocationRepository locationRepository, Course? course) : base(course)
         {
+            _courseRepository = courseRepository ?? throw new ArgumentNullException(nameof(courseRepository));
+            _dialogService = dialogService ?? throw new ArgumentNullException(nameof(dialogService));
+            _locationRepository = locationRepository ?? throw new ArgumentNullException(nameof(locationRepository));
+
+            Course = course;
+            SaveCommand = new RelayCommand(async () => await OnSaveAsync());
+            CancelCommandd = new RelayCommand( OnCancel);
 
            
+        }
+
+        public CourseDialogViewModel(Course? dialogResultType) : base(dialogResultType)
+        {
         }
 
         protected override void InvokeResponseCallback(DialogResult<Course> dialogResult)
@@ -27,5 +49,22 @@ namespace CoursesManager.UI.ViewModels.Students
             ResponseCallback.Invoke(dialogResult);
         }
 
+        private async Task OnSaveAsync()
+        {
+
+        }
+
+        public void OnCancel()
+        {
+            var dialogResult = DialogResult<bool>.Builder()
+                .SetSuccess(false, "Operation Cancelled")
+                .Build();
+            CloseDialogWithResult(dialogResult);
+        }
+
+        private void CloseDialogWithResult(DialogResult<bool> dialogResult)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
