d95fe7e5a4ab014785b8c76dc4ade516b0c0ad55
BasWindesheim <152599986+BasWindesheim@users.noreply.github.com>
2024-11-20 20:48:41 +0100

    paar aanpassingen voor een dialog venster (lukt nog niet)

--- CoursesManager.UI/Models/Course.cs
+++ CoursesManager.UI/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows.Controls;
 
 namespace CoursesManager.UI.Models
 {
@@ -24,6 +25,8 @@ namespace CoursesManager.UI.Models
         public Location? Location { get; set; }
         public DateTime DateCreated { get; set; }
         public ObservableCollection<Student>? students { get; set; }
+
+        public Image Image { get; set; }
 
         public string GenerateFilterString()
         {
