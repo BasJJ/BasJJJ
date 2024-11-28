19a6e12beefea52a9edbe5675a4e90400aa367c2
Jarno Gerrets <jarnogerrets@gmail.com>
2024-11-28 15:44:41 +0100

    changed  "some" things

--- CoursesManager.UI/Models/Course.cs
+++ CoursesManager.UI/Models/Course.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace CoursesManager.UI.Models
 {
@@ -19,6 +21,8 @@ namespace CoursesManager.UI.Models
         public Location? Location { get; set; }
         public DateTime DateCreated { get; set; }
         public ObservableCollection<Student>? students { get; set; }
+
+        public BitmapImage? Image { get; set; }
 
         public string GenerateFilterString()
         {
