7214977537d3fb3b30f95e4572c463368852ec11
Jarne Kompier <jko@wigersma-sikkema.com>
2024-11-30 12:53:00 +0100

    wip

---
+++ CoursesManager.UI/Database/Seed/Location_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO courses_manager.locations (id, name, adress_id, created_at, updated_at) VALUES (1, 'Windesheim in Almere', 2, '2024-11-26 19:49:38', '2024-11-26 19:49:38');
+INSERT INTO courses_manager.locations (id, name, adress_id, created_at, updated_at) VALUES (2, 'Windesheim University of Applied Sciences', 1, '2024-11-26 19:52:05', '2024-11-26 19:49:38');
