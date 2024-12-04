252a23347d516fc201cbfdea4f81c8983666cd20
Shadi <Shadi.haddad@spacewell.com>
2024-12-04 23:39:19 +0100

    - Student & Address model fix - Student getters and add queries is added - Address queries is added - Student and address repositories calls the methods in the Data access.

--- CoursesManager.UI/DataAccess/AddressDataAccess.cs
+++ CoursesManager.UI/DataAccess/AddressDataAccess.cs
@@ -1,43 +1,131 @@
 ﻿using CoursesManager.UI.Models;
 using MySql.Data.MySqlClient;
+using System.Collections.Generic;
 
-namespace CoursesManager.UI.DataAccess;
-
-public class AddressDataAccess : BaseDataAccess<Address>
+namespace CoursesManager.UI.DataAccess
 {
-    public void Add(Address address)
+    public class AddressDataAccess : BaseDataAccess<Address>
     {
-        try
+        
+        public List<Address> GetAll()
         {
-            throw new NotImplementedException();
+            string query = "SELECT * FROM addresses";
+            return FetchAll(query);
         }
-        catch (MySqlException ex)
-        {
-            throw new InvalidOperationException(ex.Message, ex);
-        }
-    }
 
-    public void Update(Address address)
-    {
-        try
+        public Address? GetById(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                string query = "SELECT * FROM addresses WHERE id = @Id";
+                var parameters = new MySqlParameter[] { new MySqlParameter("@Id", id) };
+                var addresses = FetchAll(query, parameters);
+                return addresses.FirstOrDefault();
+            }
+            catch (MySqlException ex)
+            {
+                throw new InvalidOperationException(ex.Message, ex);
+            }
         }
-        catch (MySqlException ex)
-        {
-            throw new InvalidOperationException(ex.Message, ex);
-        }
-    }
 
-    public void Delete(int id)
-    {
-        try
+        public void Add(Address address)
         {
-            throw new NotImplementedException();
+            try
+            {
+                string query = "INSERT INTO addresses (country, zipcode, city, street, house_number, house_number_extension, created_at, updated_at) " +
+                               "VALUES (@Country, @ZipCode, @City, @Street, @HouseNumber, @HouseNumberExtension, @CreatedAt, @UpdatedAt)";
+
+                var parameters = new Dictionary<string, object>
+                {
+                    { "@Country", address.Country },
+                    { "@ZipCode", address.ZipCode },
+                    { "@City", address.City },
+                    { "@Street", address.Street },
+                    { "@HouseNumber", address.HouseNumber },
+                    { "@HouseNumberExtension", address.HouseNumberExtension ?? (object)DBNull.Value },
+                    { "@CreatedAt", DateTime.Now },
+                    { "@UpdatedAt", DateTime.Now }
+                };
+
+                ExecuteNonQuery(query, parameters);
+            }
+            catch (MySqlException ex)
+            {
+                throw new InvalidOperationException(ex.Message, ex);
+            }
         }
-        catch (MySqlException ex)
+
+        public int GetLastInsertedId()
         {
-            throw new InvalidOperationException(ex.Message, ex);
+            try
+            {
+                string query = "SELECT LAST_INSERT_ID()";
+                using var connection = GetConnection();
+                using var command = new MySqlCommand(query, connection);
+                connection.Open();
+                return Convert.ToInt32(command.ExecuteScalar());
+            }
+            catch (MySqlException ex)
+            {
+                throw new InvalidOperationException(ex.Message, ex);
+            }
+        }
+
+        public void Update(Address address)
+        {
+            try
+            {
+                string query = "UPDATE addresses SET country = @Country, zipcode = @ZipCode, city = @City, street = @Street, " +
+                               "house_number = @HouseNumber, house_number_extension = @HouseNumberExtension, updated_at = @UpdatedAt WHERE id = @Id";
+
+                var parameters = new Dictionary<string, object>
+                {
+                    { "@Country", address.Country },
+                    { "@ZipCode", address.ZipCode },
+                    { "@City", address.City },
+                    { "@Street", address.Street },
+                    { "@HouseNumber", address.HouseNumber },
+                    { "@HouseNumberExtension", address.HouseNumberExtension ?? (object)DBNull.Value },
+                    { "@UpdatedAt", DateTime.Now },
+                    { "@Id", address.Id }
+                };
+
+                ExecuteNonQuery(query, parameters);
+            }
+            catch (MySqlException ex)
+            {
+                throw new InvalidOperationException(ex.Message, ex);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            try
+            {
+                string query = "DELETE FROM addresses WHERE id = @Id";
+                var parameters = new MySqlParameter[] { new MySqlParameter("@Id", id) };
+                ExecuteNonQuery(query, parameters);
+            }
+            catch (MySqlException ex)
+            {
+                throw new InvalidOperationException(ex.Message, ex);
+            }
+        }
+
+        protected Address FillModel(MySqlDataReader reader)
+        {
+            return new Address
+            {
+                Id = reader.GetInt32("id"),
+                Country = reader.GetString("country"),
+                ZipCode = reader.GetString("zipcode"),
+                City = reader.GetString("city"),
+                Street = reader.GetString("street"),
+                HouseNumber = reader.GetString("house_number"),
+                HouseNumberExtension = reader.IsDBNull(reader.GetOrdinal("house_number_extension")) ? null : reader.GetString("house_number_extension"),
+                CreatedAt = reader.GetDateTime("created_at"),
+                UpdatedAt = reader.GetDateTime("updated_at")
+            };
         }
     }
 }
--- CoursesManager.UI/DataAccess/StudentDataAccess.cs
+++ CoursesManager.UI/DataAccess/StudentDataAccess.cs
@@ -1,7 +1,149 @@
 ﻿using CoursesManager.UI.Models;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace CoursesManager.UI.DataAccess;
-
-public class StudentDataAccess : BaseDataAccess<Student>
+namespace CoursesManager.UI.DataAccess
 {
+    public class StudentDataAccess : BaseDataAccess<Student>
+    {
+        private readonly AddressDataAccess _addressDataAccess;
+
+        public StudentDataAccess()
+        {
+            _addressDataAccess = new AddressDataAccess();
+        }
+
+        public List<Student> GetAll()
+        {
+            try
+            {
+                string query = "SELECT * FROM students";
+                return FetchAll(query);
+            }
+            catch (MySqlException ex)
+            {
+                throw new InvalidOperationException(ex.Message, ex);
+            }
+        }
+
+        public List<Student> GetNotDeletedStudents()
+        {
+            try
+            {
+                string query = "SELECT * FROM students WHERE is_deleted = 0";
+                return FetchAll(query);
+            }
+            catch (MySqlException ex)
+            {
+                throw new InvalidOperationException(ex.Message, ex);
+            }
+        }
+
+        public List<Student> GetDeletedStudents()
+        {
+            try
+            {
+                string query = "SELECT * FROM students WHERE is_deleted = 1";
+                return FetchAll(query);
+            }
+            catch (MySqlException ex)
+            {
+                throw new InvalidOperationException(ex.Message, ex);
+            }
+        }
+
+        public void Add(Student student)
+        {
+            try
+            {
+                // Add the address first
+                _addressDataAccess.Add(student.Address);
+
+                // Get the newly created address ID
+                int addressId = _addressDataAccess.GetLastInsertedId();
+
+                string query = "INSERT INTO students (first_name, last_name, email, phone, address_id, is_deleted, deleted_at, created_at, updated_at, insertion, date_of_birth) " +
+                               "VALUES (@FirstName, @LastName, @Email, @Phone, @AddressId, @IsDeleted, @DeletedAt, @CreatedAt, @UpdatedAt, @Insertion, @DateOfBirth)";
+
+                var parameters = new Dictionary<string, object>
+                {
+                    { "@FirstName", student.FirstName },
+                    { "@LastName", student.LastName },
+                    { "@Email", student.Email },
+                    { "@Phone", student.Phone },
+                    { "@AddressId", addressId },
+                    { "@IsDeleted", student.IsDeleted },
+                    { "@DeletedAt", student.DeletedAt ?? (object)DBNull.Value },
+                    { "@CreatedAt", DateTime.Now },
+                    { "@UpdatedAt", DateTime.Now },
+                    { "@Insertion", student.Insertion ?? (object)DBNull.Value },
+                    { "@DateOfBirth", student.DateOfBirth }
+                };
+
+                ExecuteNonQuery(query, parameters);
+            }
+            catch (MySqlException ex)
+            {
+                throw new InvalidOperationException(ex.Message, ex);
+            }
+        }
+
+        public Student? GetById(int id)
+        {
+            try
+            {
+                string query = "SELECT * FROM students WHERE id = @Id";
+                var parameters = new MySqlParameter[] { new MySqlParameter("@Id", id) };
+                var students = FetchAll(query, parameters);
+                return students.FirstOrDefault();
+            }
+            catch (MySqlException ex)
+            {
+                throw new InvalidOperationException(ex.Message, ex);
+            }
+        }
+
+        public void Delete(Student student)
+        {
+            ArgumentNullException.ThrowIfNull(student);
+
+            throw new NotImplementedException();
+        }
+
+        public void DeleteById(int id)
+        {
+            ArgumentNullException.ThrowIfNull(id);
+
+            throw new NotImplementedException();
+        }
+
+        protected override Student FillModel(MySqlDataReader reader)
+        {
+            var student = new Student
+            {
+                Id = reader.GetInt32("id"),
+                FirstName = reader.GetString("first_name"),
+                LastName = reader.GetString("last_name"),
+                Email = reader.GetString("email"),
+                Phone = reader.GetString("phone"),
+                IsDeleted = reader.GetBoolean("is_deleted"),
+                DeletedAt = reader.IsDBNull(reader.GetOrdinal("deleted_at")) ? (DateTime?)null : reader.GetDateTime("deleted_at"),
+                CreatedAt = reader.GetDateTime("created_at"),
+                UpdatedAt = reader.GetDateTime("updated_at"),
+                AddressId = reader.IsDBNull(reader.GetOrdinal("address_id")) ? (int?)null : reader.GetInt32("address_id"),
+                DateOfBirth = reader.GetDateTime("date_of_birth"),
+                Insertion = reader.IsDBNull(reader.GetOrdinal("insertion")) ? null : reader.GetString("insertion")
+            };
+
+            if (student.AddressId.HasValue)
+            {
+                student.Address = _addressDataAccess.FetchOneById(student.AddressId.Value);
+            }
+
+            return student;
+        }
+    }
 }
+
+
