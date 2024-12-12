515cd4e456522a243502e9ee7af8ba96e2eb2ab2
ygroenendal <s1204320@student.windesheim.nl>
2024-12-12 17:33:33 +0100

    unit test gefixed

--- CoursesManager.Tests/Student/AddStudentTests.cs
+++ CoursesManager.Tests/Student/AddStudentTests.cs
@@ -56,8 +56,8 @@ namespace CoursesManager.Tests
                 .Setup(repo => repo.GetAll())
                 .Returns(new List<Course>
                 {
-                    new Course { Id = 1, Name = "Math" },
-                    new Course { Id = 2, Name = "Science" }
+                    new Course { Id = 1, Name = "Math", IsActive = true },
+                    new Course { Id = 2, Name = "Science", IsActive = true}
                 });
 
             _mockRegistrationRepository
